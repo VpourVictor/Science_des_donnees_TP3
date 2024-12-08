01a0382e0cacccf3a523976ed235b4dbada60bd2
Victor Levrel <levv1279@USherbrooke.ca>
2024-12-08 10:42:27 -0500

    [feat] : visualisation of movie genres

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ import numpy as np
 from scipy.spatial import distance
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.decomposition import PCA
+from collections import defaultdict
 
 
 
@@ -35,8 +36,66 @@ def init_traitement_DB(data_path) :
     """
     #Lecture du fichier
     df = pd.read_csv(data_path)
-    
-    # Suppression de la première colonne inutile
-    df = df.iloc[:,1:]
 
     return df
+
+
+#__________________________________________________________________________________
+# Function first question
+
+def process_movies(df_movies):
+    """
+    Process the data from the movies csv file to get the number of movies per genre
+
+    Parameters
+    ----------
+        df_movies : pandas.DataFrame, the data from the movies csv file
+
+    Output
+    ------
+        genre_counts : pandas.Series, the number of movies per genre
+    """
+
+    # Ignore movies without genre
+    df_movies = df_movies[df_movies['genres'] != '(no genres listed)']
+
+    # Initialiser un dictionnaire pour compter les genres
+    genre_counts = defaultdict(int)
+
+    # Explore the dataframe
+    for genres in df_movies['genres']:
+        for genre in genres.split('|'):
+            genre_counts[genre] += 1
+
+    genre_counts = pd.Series(genre_counts)
+
+    return genre_counts
+
+def visualize_genre_distribution(df_movies, values=False, save=False, save_path='../output/genre_distribution.png'):
+    """
+    Visualize the distribution of movies per genre
+
+    Parameters
+    ----------
+        df_movies : pandas.DataFrame, the data from the movies csv file
+        values : bool, whether to display the values on the bars
+        save : bool, whether to save the plot
+        save_path : str, the path to save the plot
+    """
+
+    genre_counts = process_movies(df_movies)
+    plt.figure(figsize=(12, 8))
+    ax = genre_counts.plot(kind='bar', color='orange', zorder=3)
+    plt.title('Nombre de films par genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Nombre de films')
+    plt.xticks(rotation=45)
+    plt.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.3, zorder=0)
+    if values:
+        for i in range(len(genre_counts)):
+            ax.text(i, genre_counts[i], genre_counts[i], ha='center', va='bottom')
+
+    if save :
+        plt.savefig(save_path)
+
+    plt.show()
