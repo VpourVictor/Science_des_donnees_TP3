7fd2629e40e0903199112d17fa5b24f2128197c5
Victor Levrel <levv1279@USherbrooke.ca>
2024-12-08 11:12:31 -0500

    [feat] : Q2 clean and redo ratings into movies1.csv and ratings1.csv files

--- src/utils.py
+++ src/utils.py
@@ -71,7 +71,7 @@ def process_movies(df_movies):
 
     return genre_counts
 
-def visualize_genre_distribution(df_movies, values=False, save=False, save_path='../output/genre_distribution.png'):
+def visualize_genre_distribution(df_movies, values=False, save=False, save_path='../output/genre_distribution.png', title='Distribution des films par genre'):
     """
     Visualize the distribution of movies per genre
 
@@ -81,12 +81,13 @@ def visualize_genre_distribution(df_movies, values=False, save=False, save_path=
         values : bool, whether to display the values on the bars
         save : bool, whether to save the plot
         save_path : str, the path to save the plot
+        title : str, the title of the plot
     """
 
     genre_counts = process_movies(df_movies)
     plt.figure(figsize=(12, 8))
     ax = genre_counts.plot(kind='bar', color='orange', zorder=3)
-    plt.title('Nombre de films par genre')
+    plt.title(title)
     plt.xlabel('Genre')
     plt.ylabel('Nombre de films')
     plt.xticks(rotation=45)
@@ -95,7 +96,36 @@ def visualize_genre_distribution(df_movies, values=False, save=False, save_path=
         for i in range(len(genre_counts)):
             ax.text(i, genre_counts[i], genre_counts[i], ha='center', va='bottom')
 
-    if save :
+    if save:
         plt.savefig(save_path)
 
     plt.show()
+
+
+def clean_and_save_data(df_movies, df_ratings):
+    """
+    Clean the data and save it to new csv files
+
+    Parameters
+    ----------
+        df_movies : pandas.DataFrame, the data from the movies csv file
+        df_ratings : pandas.DataFrame, the data from the ratings csv file
+    """
+    
+    df_movies = df_movies[df_movies['genres'] != '(no genres listed)']
+    listed_movie_ids = df_movies['movieId'].unique()
+    df_ratings = df_ratings[df_ratings['movieId'].isin(listed_movie_ids)]
+
+    # Adjusting rating values
+    df_ratings.loc[df_ratings['rating'] == 5.5, 'rating'] = 5
+    df_ratings.loc[df_ratings['rating'] == 4.5, 'rating'] = 4
+    df_ratings.loc[df_ratings['rating'] == 3.5, 'rating'] = 3
+    df_ratings.loc[df_ratings['rating'] == 2.5, 'rating'] = 2
+    df_ratings.loc[df_ratings['rating'] == 1.5, 'rating'] = 1
+    df_ratings.loc[df_ratings['rating'] == 0.5, 'rating'] = 1
+
+    # save the cleaned data
+    df_movies.to_csv('../data/movies1.csv', index=False)
+    df_ratings.to_csv('../data/ratings1.csv', index=False)
+
+    print("Les fichiers movies1.csv et ratings1.csv ont été créés avec succès.")
